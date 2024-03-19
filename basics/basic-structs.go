550d5602827a4f5eed9a3619fd8c83c016b0985f
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-03-19 19:59:11 -0300

    Adding advanced struct 2

--- basics/basic-structs.go
+++ basics/basic-structs.go
@@ -2,12 +2,19 @@ package main
 
 import "fmt"
 
-// struct syntax just like C
 
+// struct syntax just like C
+// You can use embbeded structs to represent more complex types
+type Endereco struct {
+	Rua string
+	CEP string
+	Cidade string
+}
 type Pessoa struct {
 	Nome  string
 	Idade int
 	Email string
+	Endereco
 }
 
 // methods can me declared to structs and be called like:
@@ -45,6 +52,19 @@ func (c Cachorro) SoundEmmit() string {
 	return "\nBarking!!"
 }
 
+// you can create interface with methods and "implements into other structs"
+type FormaGeometricaa interface {
+	Area() float64
+}
+
+type Retangulo struct {
+	Largura float64
+	Altura float64
+}
+
+func (r Retangulo) Area() float64  {
+	return r.Largura * r.Altura
+}
 func main() {
 	pessoa := Pessoa{Nome: "Felipe", Idade: 24, Email: "teste@email.com"}
 	fmt.Println("Created person")
@@ -54,8 +74,29 @@ func main() {
 	fmt.Println("Presenting")
 	pessoa.Apresentar()
 
-	fmt.Println("Interface usage")
+	fmt.Println("\nInterface usage")
 	var animal Animal
 	animal = Cachorro{Nome: "Bolinha"}
 	fmt.Println(animal.SoundEmmit())
+
+	pessoa2 := Pessoa{
+		Nome: "Maria",
+		Idade: 25,
+		Email: "maria@email.com",
+		Endereco: Endereco{
+			Rua: "Rua da rua",
+			CEP: "123456-123",
+			Cidade: "Aquela cidade",
+		},
+	}
+	fmt.Println("Embbeded structs")
+	fmt.Printf("%s mora em %s", pessoa2.Nome, pessoa2.Cidade)
+
+
+	// interface methods
+	retangulo := Retangulo{Largura: 10, Altura: 5}
+	fmt.Println(retangulo.Area()) // Saída: 50.0
+
+
+
 }
