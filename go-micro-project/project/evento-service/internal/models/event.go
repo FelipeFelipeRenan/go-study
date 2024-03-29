1391a8c87d092949363d6a387e987e9f843d4f69
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-03-29 12:49:45 -0300

    [DEV] Adding more fields to event model

--- go-micro-project/project/evento-service/internal/models/event.go
+++ go-micro-project/project/evento-service/internal/models/event.go
@@ -6,5 +6,8 @@ type Event struct {
 	gorm.Model
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	Location    string `json:"location"`
+	StartAt     string `json:"start_at"`
+	EndAt       string `json:"end_at"`
 	// Adicionar outros campos conforme necessário
 }
