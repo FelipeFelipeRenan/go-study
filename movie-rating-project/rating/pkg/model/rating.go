eeb276bd65d1bae4d64072f65e0c2b8548b182e6
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-04-14 15:54:22 -0300

    [DEV] Adding model to rating service

---
+++ movie-rating-project/rating/pkg/model/rating.go
@@ -0,0 +1,30 @@
+package model
+
+// RecordID defines a record id. Together with RecordType
+// identifies unique records across all types.
+type RecordID string
+
+// RecordType defines a record type. Together with RecordID
+// identifies unique record across all types
+type RecordType string
+
+// Existing record types
+const(
+	RecordTypeMovie = RecordType("movie")
+)
+
+// UserID defines a user id.
+type UserID string
+
+// RatingValue defines a value of a rating record
+type RatingValue int
+
+// Rating defines an individual rating created by a user for
+// some record
+
+type Rating struct {
+	RecordID string `json:"recorddId"`
+	RecordType string `json:"recordType"`
+	UserID string `json:"userId"`
+	Value RatingValue `json:"value"` 
+}
