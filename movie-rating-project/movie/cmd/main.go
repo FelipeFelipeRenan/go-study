d243ce31e35b242d52e9a837423e865a0f7966a9
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-04-15 15:29:02 -0300

    [DEV] Adding main file to movie service

--- movie-rating-project/movie/cmd/main.go
+++ movie-rating-project/movie/cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"movieexample/movie/internal/controller/movie"
+	metadatagateway "movieexample/movie/internal/gateway/metadata/http"
+	ratinggateway "movieexample/movie/internal/gateway/rating/http"
+	httphandler "movieexample/movie/internal/handler/http"
+	"net/http"
+)
+
 func main() {
-	
+	log.Println("Starting the movie service")
+	metadataGateway := metadatagateway.New("localhosy:8081")
+	ratingGateway := ratinggateway.New("localhost:8082")
+
+	ctrl := movie.New(ratingGateway, metadataGateway)
+	h := httphandler.New(ctrl)
+	http.Handle("/movie", http.HandlerFunc(h.GetMovieDetails))
+	if err := http.ListenAndServe(":8083", nil); err != nil{
+		panic(err)
+	}
 }
