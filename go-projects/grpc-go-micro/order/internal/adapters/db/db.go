3cb99576b57ba2130f6ad8a4ab715cc382a649f8
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-04-20 13:44:37 -0300

    [DEV

--- go-projects/grpc-go-micro/order/internal/adapters/db/db.go
+++ go-projects/grpc-go-micro/order/internal/adapters/db/db.go
@@ -1,6 +1,11 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 type Order struct {
 	gorm.Model
@@ -19,4 +24,17 @@ type OrderItem struct {
 
 type Adapter struct {
 	db *gorm.DB	
+}
+
+func NewAdapter(dataSourceUrl string)(*Adapter, error)  {
+	db, openErr := gorm.Open(mysql.Open(dataSourceUrl), &gorm.Config{})
+	if openErr != nil{
+		return nil, fmt.Errorf("db connection error: %v", openErr)
+	}
+	
+	err := db.AutoMigrate(&Order{}, OrderItem{})
+	if err != nil {
+		return nil, fmt.Errorf("db migration error: %v", err)
+	}
+	return &Adapter{db: db}, nil
 }
