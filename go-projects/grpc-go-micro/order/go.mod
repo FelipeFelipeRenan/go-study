204cfac9ec0af4e1093790f51102753fb1a0dc72
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-04-23 12:26:25 -0300

    [DEV] Adding Create order to adapter

--- go-projects/grpc-go-micro/order/go.mod
+++ go-projects/grpc-go-micro/order/go.mod
@@ -3,10 +3,20 @@ module order
 go 1.22.1
 
 require (
+	google.golang.org/grpc v1.43.0
+	google.golang.org/protobuf v1.27.1
+	gorm.io/driver/mysql v1.5.6
+	gorm.io/gorm v1.25.9
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/driver/mysql v1.5.6 // indirect
-	gorm.io/gorm v1.25.9 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- go-projects/grpc-go-micro/order/internal/adapters/grpc/server.go
+++ go-projects/grpc-go-micro/order/internal/adapters/grpc/server.go
@@ -1,9 +1,11 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"order/internal/application/core/domain"
 	order "order/internal/gen"
 	"order/internal/ports"
 
@@ -17,11 +19,11 @@ type Adapter struct {
 	order.UnimplementedOrderServer
 }
 
-func NewAdapter(api ports.APIPort, port int) *Adapter  {
-	return &Adapter{api: api,port: port}
+func NewAdapter(api ports.APIPort, port int) *Adapter {
+	return &Adapter{api: api, port: port}
 }
 
-func (a *Adapter) Run(){
+func (a *Adapter) Run() {
 	var err error
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
 	if err != nil {
@@ -29,10 +31,27 @@ func (a *Adapter) Run(){
 	}
 	grpcServer := grpc.NewServer()
 	order.RegisterOrderServer(grpcServer, a)
-	if config.GetEnv() == "development"{
+	if config.GetEnv() == "development" {
 		reflection.Register(grpcServer)
 	}
-	if err := grpcServer.Serve(listen); err != nil{
+	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("failed to serve grpc on port ")
 	}
+}
+
+func (a Adapter) CreateOrder(ctx context.Context, request *order.CreateOrderRequest) (*order.CreateOrderResponse, error) {
+	var orderItems []domain.OrderItem
+	for _, orderItem := range request.OrderItems{
+		orderItems = append(orderItems, domain.OrderItem{
+			ProductCode: orderItem.ProductCode,
+			UnitPrice: orderItem.UnitPrice,
+			Quantity: orderItem.Quantity,
+		})
+	}
+	newOrder := domain.NewOrder(request.UserId, orderItems)
+	result, err := a.api.PlaceOrder(newOrder)
+	if err != nil {
+		return nil, err
+	}
+	return &order.CreateOrderResponse{OrderId: result.ID}, nil
 }
